c2bc17d0138c0a749806aa647b5792992dab6431
Manuel Lopez <jmlopez@iofficecorp.com>
2018-08-28 16:42:07 -0500

    [WIP] 3 tests failing
    This may be due to the original pngs being done with another
    headless browser. I will redo all of them with chrome and see if they pass
    later on travis.

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,41 +1,36 @@
-// import * as fs from 'fs';
-// import * as path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 import { expect } from 'chai';
-import { ITest, normalizeTests } from './util';
+import { ITest, normalizeTests, pngCompare } from './util';
 import { Svg2png, svg2png } from '../main/svg2png';
 
-// const rel = (x: string) => path.resolve(__dirname, x);
+const rel = (x: string) => path.resolve(__dirname, x);
 const successTests = normalizeTests(require('../../src/test/success-tests.json'));
 
 
 describe('async', () => {
   describe('should fulfill', () => successTests.forEach(successTest));
 
-  describe('closing', () => {
-    it('should close', (done) => {
-      Svg2png.closePool().then(done, done);
-    });
+  after(async () => {
+    await Svg2png.closePool();
   });
 });
 
 
-function successTest(test: ITest, _index: number) {
-  it(test.name, (done) => {
-    // const expected = fs.readFileSync(rel(`../../src/test/success-tests/${index}.png`));
-    svg2png({
+function successTest(test: ITest, index: number) {
+  it(test.name, async () => {
+    const expected = fs.readFileSync(rel(`../../src/test/success-tests/${index}.png`));
+    const actual = await svg2png({
       url: test.file,
-    }).then(() => {
-      console.log('hello?');
-      try {
-        expect(5).to.equal(4);
-        done();
-        console.log('done...');
-      } catch (err) {
-        console.log('err:', err);
-        done(err);
-      }
-    }, done);
-
-    //
+      ...test.options,
+    });
+    const result = await pngCompare(expected, actual);
+    const ratio = result.diffPixels/result.totalPixels;
+    try {
+      expect(ratio).to.be.lessThan(0.1, 'More than 10% of the pixels are different');
+    } catch (err) {
+      result.png.pipe(fs.createWriteStream(`err_${index}.png`));
+      throw err;
+    }
   });
 }
--- src/test/util.ts
+++ src/test/util.ts
@@ -1,7 +1,12 @@
 import * as path from 'path';
+import * as fs from 'fs';
+import { Stream } from 'stream';
 import fileURL = require('file-url');
+import { ReadStream } from 'fs';
+import { PNG } from 'pngjs';
 
 const rel = (x: string) => path.resolve(__dirname, x);
+const streamifier = require('streamifier');
 
 interface ITest {
   name: string;
@@ -11,6 +16,8 @@ interface ITest {
   includeURL?: string;
   includeFilename?: string;
 }
+
+type StrBuffStream = string | Buffer | Stream;
 
 function normalizeTests(tests: ITest[]) {
   return tests.map(test => {
@@ -31,12 +38,82 @@ function normalizeTests(tests: ITest[]) {
     }
 
     normalized.file = filename;
-    console.log('normalized:', normalized);
     return normalized;
   });
 }
 
+
+function toStream(obj: StrBuffStream): Promise<ReadStream> {
+  return new Promise((resolve, reject) => {
+    let result;
+    if (typeof obj === 'string') {
+      result = fs.createReadStream(obj).once('error', reject);
+    }
+    if (obj instanceof Buffer) {
+      result = streamifier.createReadStream(obj).once('error', reject);
+    }
+    if (obj instanceof Stream) {
+      result = obj;
+    }
+    resolve(result);
+  });
+}
+
+function parsePNG(obj: StrBuffStream): Promise<PNG> {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const stream = await toStream(obj);
+      stream.pipe(new PNG())
+        .once('error', reject)
+        .on('parsed', function onParsed() {
+          resolve(this);
+        });
+    } catch (err) {
+      reject(err);
+    }
+  });
+}
+
+async function pngCompare(obj1: StrBuffStream, obj2: StrBuffStream) {
+  const png1 = await parsePNG(obj1);
+  const png2 = await parsePNG(obj2);
+  const dim1 = [png1.width, png1.height];
+  const dim2 = [png2.width, png2.height];
+  if (png1.data.length !== png2.data.length) {
+    return Promise.reject(new Error(`Dimension mismatch: [${dim1}] vs [${dim2}].`));
+  }
+
+  const writeStream = new PNG({ width: dim1[0], height: dim1[1] });
+  const data = writeStream.data;
+  let i = 0;
+  let count = 0;
+  while (png1.data[i] != null) {
+    if (
+      png1.data[i] !== png2.data[i] ||
+      png1.data[i + 1] !== png2.data[i + 1] ||
+      png1.data[i + 2] !== png2.data[i + 2] ||
+      png1.data[i + 3] !== png2.data[i + 3]
+    ) {
+      count += 1;
+      data[i] = png2.data[i];
+      data[i + 1] = png2.data[i + 1];
+      data[i + 2] = png2.data[i + 2];
+      data[i + 3] = png2.data[i + 3];
+    }
+    i += 4;
+  }
+
+  return {
+    diffPixels: count,
+    totalPixels: dim1[0]*dim1[1],
+    png: writeStream.pack(),
+  };
+}
+
+
 export {
   ITest,
   normalizeTests,
+  toStream,
+  pngCompare,
 };
