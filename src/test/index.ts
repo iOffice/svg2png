62b4fd8c640ed94e46b94fd95eaaf3fd2c099131
Manuel Lopez <jmlopez@iofficecorp.com>
2018-08-28 17:19:04 -0500

    [feat] added failure tests

--- src/test/index.ts
+++ src/test/index.ts
@@ -6,10 +6,12 @@ import { Svg2png, svg2png } from '../main/svg2png';
 
 const rel = (x: string) => path.resolve(__dirname, x);
 const successTests = normalizeTests(require('../../src/test/success-tests.json'));
+const failureTests = normalizeTests(require('../../src/test/failure-tests.json'));
 
 
 describe('async', () => {
   describe('should fulfill', () => successTests.forEach(successTest));
+  describe("should reject", () => failureTests.forEach(failureTest));
 
   after(async () => {
     await Svg2png.closePool();
@@ -27,10 +29,25 @@ function successTest(test: ITest, index: number) {
     const result = await pngCompare(expected, actual);
     const ratio = result.diffPixels/result.totalPixels;
     try {
-      expect(ratio).to.be.lessThan(0.1, 'More than 10% of the pixels are different');
+      expect(ratio).to.be.lessThan(0.01, 'More than 1% of the pixels are different');
     } catch (err) {
       result.png.pipe(fs.createWriteStream(`err_${index}.png`));
       throw err;
     }
   });
 }
+
+function failureTest(test: ITest) {
+  it(test.name, async () => {
+    try {
+      await svg2png({
+        url: test.file,
+        ...test.options,
+      });
+    } catch (err) {
+      return expect(err.message).to.include(test['expectedErrorSubstring']);
+    }
+    throw new Error('Did not get the exception');
+  });
+}
+
